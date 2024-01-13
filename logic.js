0e4ac9f95e43d12e2592a678785350241199701d
Allan Mathews <allancmathews@gmail.com>
2024-01-13 18:39:15 -0500

    legend added

--- logic.js
+++ logic.js
@@ -44,6 +44,27 @@ d3.json(url).then(function(response) {
       fillColor: getColor(location[2]),
       radius: markerSize(magnitude_level)
    }).addTo(myMap);
-    
   }
+
+
+
+let legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+    grades = [0, 10, 30, 50, 70, 90],
+    labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
 });
